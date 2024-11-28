0348d08c7646332d8b358f13bd72f5ec305f25e5
Jose Hidalgo <jhidalgoesp@gmail.com>
2024-11-28 17:17:22 -0500

    Adding references highlight

--- init.lua
+++ init.lua
@@ -8,20 +8,34 @@ local jhidalgoepsGroup = augroup('jhidalgoesp', {})
 local autocmd = vim.api.nvim_create_autocmd
 
 autocmd('LspAttach', {
-        group = jhidalgoepsGroup,
-        callback = function(e)
-            local opts = { buffer = e.buf }
-            vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-            vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-            vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-            vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-            vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-            vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-            vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-            vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-            vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-            vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-        end
+  group = jhidalgoepsGroup,
+  callback = function(e)
+    local opts = { buffer = e.buf }
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references)
+  end
+})
+
+-- Highlight references
+vim.api.nvim_create_autocmd({ "CursorHold" }, {
+  callback = function()
+    vim.lsp.buf.document_highlight()
+  end
+})
+
+vim.api.nvim_create_autocmd({ "CursorMoved" }, {
+  callback = function()
+    vim.lsp.buf.clear_references()
+  end
 })
 
 local dap, dapui =require("dap"),require("dapui")
