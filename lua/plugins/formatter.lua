98d39d997ad0cd6c68975038321e0848df841ed7
José Hidalgo Espinel <jhidalgoesp@gmail.com>
2024-12-06 12:17:09 -0500

    adding ctp formatter

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -5,20 +5,36 @@ return {
 			-- Utilities for creating configurations
 			local util = require("formatter.util")
 
-			-- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
-			require("formatter").setup({
-				-- Enable or disable logging
-				logging = true,
-				-- Set the log level
-				log_level = vim.log.levels.WARN,
-				-- All formatter configurations are opt-in
-				filetype = {
-					-- Formatter configurations for filetype "lua" go here
-					-- and will be executed in order
-					lua = {
-						-- "formatter.filetypes.lua" defines default configurations for the
-						-- "lua" filetype
-						require("formatter.filetypes.lua").stylua,
+      local php_formatter = {
+        function()
+          return {
+            exe = './node_modules/.bin/prettier',
+            args = {
+              '--stdin-filepath',
+              vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)),
+              '--config',
+              vim.fn.getcwd() .. '/.prettierrc.json',
+              '--plugin=@prettier/plugin-php',
+              '--parser=php'
+            },
+            stdin = true
+          }
+        end
+      }
+      -- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
+      require("formatter").setup({
+        -- Enable or disable logging
+        logging = true,
+        -- Set the log level
+        log_level = vim.log.levels.WARN,
+        -- All formatter configurations are opt-in
+        filetype = {
+          -- Formatter configurations for filetype "lua" go here
+          -- and will be executed in order
+          lua = {
+            -- "formatter.filetypes.lua" defines default configurations for the
+            -- "lua" filetype
+            require("formatter.filetypes.lua").stylua,
 
 						-- You can also define your own configuration
 						function()
@@ -73,22 +89,8 @@ return {
           css = {
             require("formatter.filetypes.css").prettier,
           },
-          php = {
-            function()
-              return {
-                exe = './node_modules/.bin/prettier',
-                args = {
-                  '--stdin-filepath',
-                  vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)),
-                  '--config',
-                  vim.fn.getcwd() .. '/.prettierrc.json',
-                  '--plugin=@prettier/plugin-php',
-                  '--parser=php'
-                },
-                stdin = true
-              }
-            end
-          },
+          ['php'] = php_formatter,
+          ['ctp'] = php_formatter,
           -- Use the special "*" filetype for defining formatter configurations on
           -- any filetype
           ["*"] = {
